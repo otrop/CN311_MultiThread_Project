e14de269540eb25395a889d56c1aa7496cc36d81
otrop <79354723+otrop@users.noreply.github.com>
2022-05-18 20:57:14 +0700

    Create client_display.py

---
+++ client_display.py
@@ -0,0 +1,50 @@
+from http import client
+import tkinter as tk
+
+LARGE_FONT_STYLE = ("Arial", 40,"bold")
+SMALL_FONT_STYLE = ("Arial", 16)
+LIGHT_GRAY = "#F5F5F5" 
+LABEL_COLOR = "#25265E"
+
+class Display:
+    def __init__(self):
+        self.window = tk.Tk()
+        self.window.geometry("375x667") #Size for start
+        self.window.resizable(0,0) #Size_resizeable
+        self.window.title("Test") #Title Tag in top
+
+        self.total_expression = "0"
+        self.current_expression = "0"
+
+        self.display_frame = self.create_display_frame()
+        
+        self.total_label, self.lable = self.create_display_labels()
+        self.buttons_frame = self.create_buttons_frame()
+
+    def create_display_labels(self):
+        total_label = tk.Label(self.display_frame, text=self.total_expression,
+         anchor=tk.E, bg=LIGHT_GRAY, fg=LABEL_COLOR, padx=24, font=SMALL_FONT_STYLE)#CENTER ทดไว้แก้
+        total_label.pack(expand=True, fill='both')
+
+        label = tk.Label(self.display_frame, text=self.current_expression,
+         anchor=tk.E, bg=LIGHT_GRAY, fg=LABEL_COLOR, padx=24, font=LARGE_FONT_STYLE)#CENTER ทดไว้แก้
+        label.pack(expand=True, fill='both')
+
+        return total_label, label
+        
+    def create_display_frame(self):
+        frame = tk.Frame(self.window, height=221, bg=LIGHT_GRAY)
+        frame.pack(exand=True, fill='both')
+        return frame
+
+    def create_buttons_frame(self):
+        frame = tk.Frame(self.window)
+        frame.pack(expand=True, fill='both')
+        return frame
+
+    def run(self):
+        self.window.mainloop()
+
+if __name__ == "__main__":
+    client_display = Display()
+    client_display.run()
