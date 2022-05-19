869fd3c77d65797718a287916d19f54a45c8b4a3
otrop <79354723+otrop@users.noreply.github.com>
2022-05-19 19:14:12 +0700

    Update client_display.py

--- client_display.py
+++ client_display.py
@@ -1,9 +1,18 @@
+from ast import operator
 from http import client
+from symtable import Symbol
 import tkinter as tk
+from unicodedata import digit
+
+from setuptools import Command
 
 LARGE_FONT_STYLE = ("Arial", 40,"bold")
 SMALL_FONT_STYLE = ("Arial", 16)
-LIGHT_GRAY = "#F5F5F5" 
+DIGITS_FONT_STYLE = ("Arial", 24, "bold")
+DEFUALT_FONT_STYLE = ("Arial", 20)
+OFF_WHITE = "#F8FAFF"
+LIGHT_GRAY = "#F5F5F5"
+LIGHT_BLUE = "#CCEDFF"
 LABEL_COLOR = "#25265E"
 
 class Display:
@@ -13,13 +22,34 @@ class Display:
         self.window.resizable(0,0) #Size_resizeable
         self.window.title("Test") #Title Tag in top
 
-        self.total_expression = "0"
-        self.current_expression = "0"
+        self.total_expression = ""
+        self.current_expression = ""
 
         self.display_frame = self.create_display_frame()
         
-        self.total_label, self.lable = self.create_display_labels()
+        self.total_label, self.label = self.create_display_labels()
+        
+        self.digits = {
+            7: (1, 1), 8: (1, 2), 9:(1, 3),
+            4: (2, 1), 5: (2, 2), 6:(2, 3),
+            1: (3, 1), 2: (3, 2), 3:(3, 3),
+            0: (4, 2), ".": (4, 1)
+        }
+
+        self.operations = {"/": "\u00F7", "*": "\u00D7", "-": "-", "+": "+"}
         self.buttons_frame = self.create_buttons_frame()
+        
+        self.buttons_frame.rowconfigure(0, weight=1)
+        for x in range(1,5):
+            self.buttons_frame.rowconfigure(x, weight=1)
+            self.buttons_frame.columnconfigure(x, weight=1)
+        self.create_digit_buttons()
+        self.create_operator_button()
+        self.create_special_button()
+
+    def create_special_button(self):
+        self.create_clear_button()
+        self.create_equals_button()
 
     def create_display_labels(self):
         total_label = tk.Label(self.display_frame, text=self.total_expression,
@@ -34,13 +64,69 @@ class Display:
         
     def create_display_frame(self):
         frame = tk.Frame(self.window, height=221, bg=LIGHT_GRAY)
-        frame.pack(exand=True, fill='both')
+        frame.pack(expand=True, fill='both')
         return frame
+
+    def add_to_expression(self, value):
+        self.current_expression += str(value)
+        self.update_label()
+
+    def create_digit_buttons(self):
+        for digit, grid_value in self.digits.items():
+            button = tk.Button(self.buttons_frame, text=str(digit), bg="#FFFFFF",
+             fg=LABEL_COLOR, font=DIGITS_FONT_STYLE, borderwidth=0, command=lambda x = digit: self.add_to_expression(x))
+            button.grid(row=grid_value[0], column=grid_value[1], sticky=tk.NSEW)
+
+    def append_operator(self, operator):
+        self.current_expression += operator
+        self.total_expression += self.current_expression
+        self.current_expression = ""
+        self.update_total_label()
+        self.update_label()
+
+    def create_operator_button(self):
+        i=0
+        for operator , symbol in self.operations.items():
+            button = tk.Button(self.buttons_frame, text=symbol,bg=OFF_WHITE,fg=LABEL_COLOR,font=DEFUALT_FONT_STYLE,borderwidth=0, command=lambda x=operator: self.append_operator(x))
+            button.grid(row=i,column=4,sticky=tk.NSEW)
+            i+=1
+
+    def clear(self):
+        self.current_expression = ""
+        self.total_expression = ""
+        self.update_label()
+        self.update_total_label()
+
+    def create_clear_button(self):
+        button = tk.Button(self.buttons_frame, text="C",bg=OFF_WHITE,fg=LABEL_COLOR,font=DEFUALT_FONT_STYLE,borderwidth=0, command=self.clear)
+        button.grid(row=0,column=1,columnspan=3,sticky=tk.NSEW)
+
+    def evaluate(self):
+        self.total_expression += self.current_expression
+        self.update_total_label()
+
+        self.current_expression = str(eval(self.total_expression))
+
+        self.total_expression = ""
+        self.update_label()
+
+    def create_equals_button(self):
+        button = tk.Button(self.buttons_frame, text="=",bg=LIGHT_BLUE,fg=LABEL_COLOR,font=DEFUALT_FONT_STYLE,borderwidth=0,command=self.evaluate)
+        button.grid(row=4,column=3,columnspan=2,sticky=tk.NSEW)
 
     def create_buttons_frame(self):
         frame = tk.Frame(self.window)
         frame.pack(expand=True, fill='both')
         return frame
+
+    def update_total_label(self):
+        expression = self.total_expression
+        for operator, symbol in self.operations.items():
+            expression = expression.replace(operator, f' {symbol} ')
+        self.total_label.config(text=expression)
+
+    def update_label(self):
+        self.label.config(text = self.current_expression)
 
     def run(self):
         self.window.mainloop()
